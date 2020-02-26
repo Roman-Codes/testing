3b54949183e0ef2d23f9941648a9ef0b0c80c7f6
Roman <gobkins@gmail.com>
2020-02-25 19:50:24 -0500

    original commit number 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+// import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain('Koko!');
+
+  ReactDOM.unmountComponentAtNode(div);
 });
